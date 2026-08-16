feafdf7dbec4a03f1aa27410deab28d208b51435
Tokodi Balázs <amedasu@gmail.com>
2026-08-16 15:55:50 +0200

    Add: 01 SysMon-CPU USAGE, CPU CORES

--- 01 System-Monitor/system-monitor.sh
+++ 01 System-Monitor/system-monitor.sh
@@ -1,3 +1,19 @@
 #! /bin/bash
+#====================================================
+# System monitor dashboard
+# CPU
+#====================================================
 
-echo "Test!!!"
+# --- Settings and Variables ---
+
+get_cpu_data() {
+	CPU_USAGE=$(top -bn1 | grep "Cpu(s)" | awk '{print $2}' | cut -d'%' -f1)
+	CPU_USAGE=${CPU_USAGE:-0}
+
+	CPU_CORES=$(nproc)
+
+echo "CPU usage : $CPU_USAGE"
+echo "CPU cores : $CPU_CORES"
+}
+
+get_cpu_data
