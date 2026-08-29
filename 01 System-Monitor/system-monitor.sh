951aed081582b3c863d0c882afe7cb40a2143813
tokodib <amedasu@gmail.com>
2026-08-29 07:36:39 +0200

    Add: Create HTML file

--- 01 System-Monitor/system-monitor.sh
+++ 01 System-Monitor/system-monitor.sh
@@ -7,6 +7,25 @@
 # --- Settings and Variables ---
 
 #====================================================
+# Configuration
+#====================================================
+
+REPORT_DIR="reports" 
+REPORT_FILE="$REPORT_DIR/system_report_$(date +%Y%m%d_%H%M).html"
+LATEST_LINK="$REPORT_DIR/index.html"
+LOG_DIR="logs"
+LOG_FILE="$LOG_DIR/monitor.log"
+HISTORY_FILE="$LOG_DIR/history.csv"
+RETENTION_DAYS=30
+INTERFACE=$(ip route | awk '/default/ {print $5; exit}')
+
+mkdir -p "$REPORT_DIR" "$LOG_DIR"
+
+log() {
+	echo "[$(date '+%Y-%m-%d %H:%M:%S')] $1" | tee -a "$LOG_FILE"
+}
+
+#====================================================
 # CPU
 #====================================================
 
@@ -87,11 +106,93 @@ get_network_data() {
     INTERFACES=$(ip -s link show | grep -E '^[0-9]+:' | awk '{print $2}' | sed 's/://')
     
 }
+
+#====================================================
+# GENERATE HTML report
+#====================================================
+cat > "$REPORT_FILE" << 'HTMLEOF'
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>System Monitor Report - HOSTNAME_PLACEHOLDER</title>
+    </head>
+    <body>
+        <h1>System Monitor Report</h1>
+        <h2>Hostname: HOSTNAME_PLACEHOLDER</h2>
+		<h3>Report generated on: DATE_PLACEHOLDER</h3>
+        <p>Uptime: UPTIME_PLACEHOLDER</p>
+        <p>OS: OS_PLACEHOLDER</p>
+        <p>Kernel: KERNEL_PLACEHOLDER</p>
+        <p>User: USER_PLACEHOLDER</p>
+
+        <h2>CPU Information</h2>
+        <p>CPU name: CPU_NAME_PLACEHOLDER</p>
+        <p>CPU Usage: CPU_USAGE_PLACEHOLDER%</p>
+        <p>CPU Core: CPU_CORE_PLACEHOLDER</p>
+        <p>Load average 1 min: LOAD_AVERAGE_1_MIN_PLACEHOLDER%</p>
+        <p>Load average 5 min: LOAD_AVERAGE_5_MIN_PLACEHOLDER%</p>
+        <p>Load average 15 min: LOAD_AVERAGE_15_MIN_PLACEHOLDER%</p>
+        <p>TOP 5 processes: TOP_5_PROCESSES_PLACEHOLDER</p>
+
+        <h2>Memory Information</h2>
+        <p>Total Memory: TOTAL_MEMORY_PLACEHOLDER MB</p>
+        <p>Used Memory: USED_MEMORY_PLACEHOLDER MB</p>
+        <p>Free Memory: FREE_MEMORY_PLACEHOLDER MB</p>
+        <p>Available Memory: AVAILABLE_MEMORY_PLACEHOLDER MB</p>
+        <p>Buffered Memory: BUFFERED_MEMORY_PLACEHOLDER MB</p>
+        <p>Used Memory (%): USED_MEMORY_PERCENTAGE_PLACEHOLDER%</p>
+        <p>TOP 5 Memory Processes: TOP_5_MEMORY_PROCESSES_PLACEHOLDER</p>
+        
+        <h2>Swap Information</h2>
+        <p>Total Swap: TOTAL_SWAP_PLACEHOLDER MB</p>
+        <p>Used Swap: USED_SWAP_PLACEHOLDER MB</p>
+        <p>Free Swap: FREE_SWAP_PLACEHOLDER MB</p>
+        <p>Percentage Swap (%): PERCENTAGE_SWAP_PLACEHOLDER%</p>
+
+        <h2>Disk Usage</h2>
+        <p>Total Disk Space: TOTAL_DISK_PLACEHOLDER GB</p>
+        <p>Used Disk Space: USED_DISK_PLACEHOLDER GB</p>
+        <p>Free Disk Space: FREE_DISK_PLACEHOLDER GB</p>
+
+        <h2>Network Information</h2>
+        <p>Default interface: DEFAULT_INTERFACE_PLACEHOLDER</p>
+        <p>RX Speed: RX_SPEED_PLACEHOLDER Mbps</p>
+        <p>TX Speed: TX_SPEED_PLACEHOLDER Mbps</p>
+        <p>RX Total: RX_TOTAL_PLACEHOLDER MB</p>
+        <p>TX Total: TX_TOTAL_PLACEHOLDER MB</p>
+
+        <p>Connections: CONNECTIONS_PLACEHOLDER</p>
+        <p>Open ports: OPEN_PORTS_PLACEHOLDER</p>
+        <p>Interfaces: INTERFACES_PLACEHOLDER</p>
+
+
+        <footer>
+            <br>
+            <p>&copy; 2026 System Monitor - TokodiB</p>
+        </footer>
+	</body>
+</html>
+HTMLEOF
+
+
 #====================================================
 # Print System informations
 #====================================================
+
+echo "Report properities:"
+echo "REPORT_DIR : $REPORT_DIR" 
+echo "REPORT_FILE : $REPORT_FILE" 
+echo "LATEST_LINK : $LATEST_LINK" 
+echo "LOG_DIR : $LOG_DIR" 
+echo "LOG_FILE : $LOG_FILE"
+echo "HISTORY_FILE : $HISTORY_FILE" 
+echo "RETENTION_DAYS : $RETENTION_DAYS"
+echo "DEFAULT INTERFACE: $INTERFACE" 
+
 get_cpu_data
-echo "CPU Info:"
+echo -e "\nCPU Info:"
 echo "CPU name: $CPU_MODEL"
 echo "CPU usage : $CPU_USAGE %"
 echo "CPU cores : $CPU_CORES"
@@ -143,4 +244,47 @@ echo "Kernel: $KERNEL"
 echo "User: $USER"
 echo "Generated: $TIMESTAMP"
 
-#echo -e "Echo test: \nHostname: $HOSTNAME \nUptime: $UPTIME \nOS: $OS \nKernel: $KERNEL \nGenerated: $TIMESTAMP"
+#====================================================
+# Change placeholders in the HTML report
+#====================================================
+sed -i "s/HOSTNAME_PLACEHOLDER/$HOSTNAME/g" "$REPORT_FILE"
+sed -i "s/DATE_PLACEHOLDER/$TIMESTAMP/g" "$REPORT_FILE"
+sed -i "s/UPTIME_PLACEHOLDER/$UPTIME/g" "$REPORT_FILE"
+sed -i "s/OS_PLACEHOLDER/$OS/g" "$REPORT_FILE"
+sed -i "s/KERNEL_PLACEHOLDER/$KERNEL/g" "$REPORT_FILE"
+sed -i "s/USER_PLACEHOLDER/$USER/g" "$REPORT_FILE"
+sed -i "s/CPU_NAME_PLACEHOLDER/$CPU_MODEL/g" "$REPORT_FILE"
+sed -i "s/CPU_USAGE_PLACEHOLDER/$CPU_USAGE/g" "$REPORT_FILE"
+sed -i "s/CPU_CORE_PLACEHOLDER/$CPU_CORES/g" "$REPORT_FILE"
+sed -i "s/LOAD_AVERAGE_1_MIN_PLACEHOLDER/$LOAD_1/g" "$REPORT_FILE"
+sed -i "s/LOAD_AVERAGE_5_MIN_PLACEHOLDER/$LOAD_5/g" "$REPORT_FILE"
+sed -i "s/LOAD_AVERAGE_15_MIN_PLACEHOLDER/$LOAD_15/g" "$REPORT_FILE"
+
+sed -i "s/TOTAL_MEMORY_PLACEHOLDER/$MEM_TOTAL/g" "$REPORT_FILE"
+sed -i "s/USED_MEMORY_PLACEHOLDER/$MEM_USED/g" "$REPORT_FILE"
+sed -i "s/FREE_MEMORY_PLACEHOLDER/$MEM_FREE/g" "$REPORT_FILE"
+sed -i "s/AVAILABLE_MEMORY_PLACEHOLDER/$MEM_AVAILABLE/g" "$REPORT_FILE"
+sed -i "s/BUFFERED_MEMORY_PLACEHOLDER/$MEM_BUFFERS/g" "$REPORT_FILE"
+sed -i "s/USED_MEMORY_PERCENTAGE_PLACEHOLDER/$MEM_PERCENT/g" "$REPORT_FILE"
+sed -i "s/TOTAL_SWAP_PLACEHOLDER/$SWAP_TOTAL/g" "$REPORT_FILE"
+sed -i "s/USED_SWAP_PLACEHOLDER/$SWAP_USED/g" "$REPORT_FILE"
+sed -i "s/FREE_SWAP_PLACEHOLDER/$SWAP_FREE/g" "$REPORT_FILE"
+sed -i "s/PERCENTAGE_SWAP_PLACEHOLDER/$SWAP_PERCENT/g" "$REPORT_FILE"
+sed -i "s/RX_SPEED_PLACEHOLDER/$RX_SPEED/g" "$REPORT_FILE"
+sed -i "s/TX_SPEED_PLACEHOLDER/$TX_SPEED/g" "$REPORT_FILE"
+sed -i "s/RX_TOTAL_PLACEHOLDER/$RX_TOTAL/g" "$REPORT_FILE"
+sed -i "s/TX_TOTAL_PLACEHOLDER/$TX_TOTAL/g" "$REPORT_FILE"
+sed -i "s/CONNECTIONS_PLACEHOLDER/$CONNECTIONS/g" "$REPORT_FILE"
+sed -i "s/DEFAULT_INTERFACE_PLACEHOLDER/$INTERFACE/g" "$REPORT_FILE"
+sed -i "s/TOTAL_DISK_PLACEHOLDER/$TOTAL_DISK/g" "$REPORT_FILE"
+sed -i "s/USED_DISK_PLACEHOLDER/$USED_DISK/g" "$REPORT_FILE"
+sed -i "s/FREE_DISK_PLACEHOLDER/$FREE_DISK/g" "$REPORT_FILE"
+
+#sed -i "s/INTERFACES_PLACEHOLDER/$INTERFACES/g" "$REPORT_FILE"
+#sed -i "s/TOP_5_PROCESSES_PLACEHOLDER/$TOP_CPU/g" "$REPORT_FILE"
+#sed -i "s/TOP_5_MEMORY_PROCESSES_PLACEHOLDER/$TOP_RAM/g" "$REPORT_FILE"
+#sed -i "s/OPEN_PORTS_PLACEHOLDER/$OPEN_PORTS/g" "$REPORT_FILE"
+
+# --- Create index.html for actual report ---
+cp "$REPORT_FILE" "$LATEST_LINK"
+
